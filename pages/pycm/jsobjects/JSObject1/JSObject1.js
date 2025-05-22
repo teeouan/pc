e2b10f8934b03a0495dc18c10b52bf4b16e8b95d
preecha saejuang <teeclubza@gmail.com>
2025-05-22 09:09:24 +0000

    3

--- pages/pycm/jsobjects/JSObject1/JSObject1.js
+++
@@ -1,8 +0,0 @@
-export default {
-	checkLogin() {
-		showAlert("isLogin = " + appsmith.store.user);  
-		if (appsmith.store.isLogin === undefined || appsmith.store.isLogin === false || appsmith.store.isLogin === "false") {
-			navigateTo('login');
-		}
-	}
-}
