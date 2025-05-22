19cf8c777be3c14ba045fc9b0e853679ea9fbccb
preecha saejuang <teeclubza@gmail.com>
2025-05-22 09:20:30 +0000

    8888

--- pages/login/jsobjects/JSObject1/JSObject1.js
+++ pages/login/jsobjects/JSObject1/JSObject1.js
@@ -1,8 +1,10 @@
 export default {
   async Button1onClick () {
-    // รัน Query1 ก่อน แล้วรอผลลัพธ์
     const res = await Query1.run();
-    if(res && res.length > 0) {
+    
+    if (res && res.length > 0) {
+      showAlert("เข้าสู่ระบบสำเร็จ", "success");
+      navigateTo("PYCM"); // เปลี่ยนชื่อหน้าตามที่คุณตั้งไว้
     } else {
       showAlert("รหัสผ่านไม่ถูกต้อง", "error");
     }
