35dd232e4d86730a8e53e8298fd09862ada0c400
preecha saejuang <teeclubza@gmail.com>
2025-05-27 07:05:51 +0000

    999

---
+++ pages/จองของ-pycm/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,8 @@
+export default {
+	checkLogin() {
+		showAlert("isLogin = " + appsmith.store.user);
+		if (appsmith.store.isLogin === undefined || appsmith.store.isLogin === false || appsmith.store.isLogin === "false") {
+			navigateTo('login');
+		}
+	}
+}
